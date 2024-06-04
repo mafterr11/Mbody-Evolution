48efd0465ab1caae63cd565809c1ac75c080a446
Alexandru Maftei <mafterr11@gmail.com>
2024-06-04 14:46:54 +0300

    .

--- src/components/servicii/Programe.jsx
+++ src/components/servicii/Programe.jsx
@@ -1,83 +1,60 @@
+import { useTranslations } from "next-intl";
 import React from "react";
 
 const Programe = () => {
+  const t = useTranslations("Servicii");
   return (
     <div className="container mb-40 grid gap-x-40 max-md:gap-y-20 md:grid-cols-2">
       <div>
         <h2 className="mb-2 max-md:text-center">ONLINE COACHING</h2>
         {/* Separator */}
-        <span className="mb-20 mt-2 block border-b-4 border-accent xl:w-[26rem] max-md:w-[90%] max-md:mx-auto" />
+        <span className="mb-20 mt-2 block border-b-4 border-accent max-md:mx-auto max-md:w-[90%] xl:w-[26rem]" />
         <div className="flex flex-col gap-y-4">
-          <p>
-            Planul alimentar, de antrenament și suplimente vor fi concepute
-            special pentru obiectivul, nevoile și particularitățile tale. De
-            asemenea, voi fi prezentă cu răspunsuri, motivație și ajustări (când
-            este cazul) pe perioada întregului proces, astfel încât să nu te
-            plafonezi, totul să fie clar și să obținem cele mai bune rezultate
-            în cel mai scurt timp.
-          </p>
+          <p>{t("online.1")}</p>
 
-          <p>Ce include: </p>
+          <p>{t("online.2")} </p>
           <ul className="flex flex-col gap-y-4">
             <li>
-              <span className="text-accent">&#9679;</span> plan alimentar
-              personalizat
+              <span className="text-accent">&#9679;</span> {t("online.3")}
             </li>
             <li>
-              <span className="text-accent">&#9679;</span> plan de antrenament
-              personalizat, în funcție de locul unde vrei să te antrenezi
-              (sala/acasă), obiectiv și disponibilitate
+              <span className="text-accent">&#9679;</span> {t("online.4")}
             </li>
             <li>
-              <span className="text-accent">&#9679;</span> exercițiile vin
-              însoțite de materiale video, unde vei vedea cum să le execuți
-              corect
+              <span className="text-accent">&#9679;</span> {t("online.5")}
             </li>
             <li>
-              <span className="text-accent">&#9679;</span> vei învață diverse
-              metode de creștere a intensității: progresive overload, drop set,
-              rest pause, s.a
+              <span className="text-accent">&#9679;</span> {t("online.6")}
             </li>
             <li>
-              <span className="text-accent">&#9679;</span> vom comunica pe
-              WhatsApp pentru eventuale întrebări pe care le aveti
+              <span className="text-accent">&#9679;</span> {t("online.7")}
             </li>
           </ul>
         </div>
       </div>
       <div>
         <h2 className="mb-2 max-md:text-center">1-1 COACHING</h2>
-            {/* Separator */}
-      <span className="mb-20 mt-2 block border-b-4 border-accent xl:w-[19rem] max-md:w-[80%] max-md:mx-auto" />
+        {/* Separator */}
+        <span className="mb-20 mt-2 block border-b-4 border-accent max-md:mx-auto max-md:w-[80%] xl:w-[19rem]" />
         <div className="flex flex-col gap-y-4">
-          <p>
-            Acesta este un serviciu pentru persoanele care-și doresc să lucreze
-            direct sub supravegherea mea, la sala 18Gym, Pipera.
-          </p>
-          <p>Ce include: </p>
+          <p>{t("one-one.1")}</p>
+          <p>{t("one-one.2")} </p>
           <ul className="flex flex-col gap-y-4">
             <li>
-              <span className="text-accent">&#9679;</span> plan de antrenament
-              personalizat, în funcție de obiectiv și disponibilitate{" "}
+              <span className="text-accent">&#9679;</span> {t("one-one.3")}{" "}
             </li>
             <li>
-              <span className="text-accent">&#9679;</span> plan alimentar
-              personalizat
+              <span className="text-accent">&#9679;</span> {t("one-one.4")}
             </li>
 
             <li>
-              <span className="text-accent">&#9679;</span> suport permanent pe
-              toată durata colaborării
+              <span className="text-accent">&#9679;</span> {t("one-one.5")}
             </li>
             <li>
-              <span className="text-accent">&#9679;</span> corectarea posturii
-              pe parcursul sesiunilor, pentru a asigura o eficienta maxima al
-              antrenamentelor
+              <span className="text-accent">&#9679;</span> {t("one-one.6")}
             </li>
             <li>
-              <span className="text-accent">&#9679;</span> vei învață diverse
-              metode de creștere a intensității: progresive overload, drop set,
-              rest pause, s.a
+              <span className="text-accent">&#9679;</span> {t("one-one.7")}
             </li>
           </ul>
         </div>
