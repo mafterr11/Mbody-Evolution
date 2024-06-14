091563c4e440fd826b57ba4a661009abf4bd5d4b
Alexandru Maftei <mafterr11@gmail.com>
2024-06-14 09:00:34 +0300

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "http://localhost:3000" || "https://mbody.vercel.app",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
